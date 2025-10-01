94df3955b3d3a0fc639cb551ea45b9c4989fbbc0
Alex-lspl <ccim595@gmail.com>
2025-10-01 13:22:43 +0300

    Разбив на несколько файлов

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?php
 require_once('config.php');
-if ($_POST) {
-    $name = trim($_POST['name']);
-    $message = trim($_POST['message']);
-
-    if ($name && $message) {
-        $stmt = $pdo->prepare("INSERT INTO messages (name, message) VALUES (?, ?)");
-        $stmt->execute([$name, $message]);
-
-        header('Location: index.php');
-        exit;
-    } else {
-        $error = "Пожалуйста, заполните все поля.";
-    }
-}
-    $stmt = $pdo->query("SELECT * FROM messages ORDER BY created_at DESC");
+$stmt = $pdo->query("SELECT * FROM messages ORDER BY created_at DESC");
     $messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
@@ -24,17 +10,7 @@ if ($_POST) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        body { font-family: Arial; margin: 40px; background: #2c2c2cff; }
-        .container { max-width: 600px; margin: auto; background: white; padding: 20px; border-radius: 8px; }
-        .message { padding: 15px; border-bottom: 1px solid #eee; }
-        .name { font-weight: bold; color: #333; }
-        .date { font-size: 0.8em; color: #888; }
-        input, textarea { width: 97%; padding: 8px; margin: 5px 0 15px; }
-        button { padding: 10px 20px; background: #7700e6ff; color: white; border: none; cursor: pointer; }
-        button:hover { background: #4f0199ff; }
-        .error { color: red; }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div class="container">
